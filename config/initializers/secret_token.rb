a71fc8cc1a4085ab81e88133aba5898f10762e38
Christopher Gillis <christopher.l.gillis@gmail.com>
2013-01-14 22:22:50 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyshirts::Application.config.secret_token = '1f41bc9d6505b3b0a6e0ddd7202875e504650ea528616f040020f1556df57237b5790273d17420f51a169399f809c9bb10afcc8937542eeb387fb6e239d3ff75'
