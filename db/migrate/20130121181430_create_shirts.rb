77912850df56c855bab50fab5e302af8b2543be5
Christopher Gillis <christopher.l.gillis@gmail.com>
2013-01-21 13:37:00 -0500

    Fixed weird scaffold error

--- db/migrate/20130121181430_create_shirts.rb
+++ db/migrate/20130121181430_create_shirts.rb
@@ -1,8 +1,8 @@
 class CreateShirts < ActiveRecord::Migration
   def change
     create_table :shirts do |t|
-      t.string, :image_url
-      t.string, :site
+      t.string :image_url
+      t.string :site
       t.string :caption
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130118045921) do
+ActiveRecord::Schema.define(:version => 20130121181430) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(:version => 20130118045921) do
   create_table "leads", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "shirts", :force => true do |t|
+    t.string   "image_url"
+    t.string   "site"
+    t.string   "caption"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
